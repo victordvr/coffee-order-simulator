66b3842bc1ba044f4263ff412ee2982adda45534
victord <victorvalencia.725@gmail.com>
2025-07-21 20:20:20 +0930

    Added 12 unit tests (9 passing, 3 failing) for Espresso, Latte, and Cappuccino

---
+++ src/test/java/com/victor/coffee/LatteTest.java
@@ -0,0 +1,33 @@
+package com.victor.coffee;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class LatteTest {
+
+    @Test
+    public void testLatteName() {
+        Latte latte = new Latte();
+        assertEquals("Latte", latte.getName());
+    }
+
+    @Test
+    public void testLattePrice() {
+        Latte latte = new Latte();
+        assertEquals(5.50, latte.getBasePrice());
+    }
+
+    @Test
+    public void testLatteIngredients() {
+        Latte latte = new Latte();
+        assertTrue(latte.getIngredients().contains("Steamed milk with a little foam"));
+    }
+
+    @Test
+    public void testLatteHasALittleOfFoam() {
+        Latte latte = new Latte();
+        assertTrue(latte.getIngredients().contains("thick layer of foam"));
+    }
+}
